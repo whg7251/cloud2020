4e6731510775323b93016f43d1b9ddb2617c7fd9
guo <whg7251@163.com>
2020-05-19 00:09:38 +0800

    log，显示是轮循到哪个payment服务

--- cloud-provider-payment8001/src/main/java/cn/guo/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/guo/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import cn.guo.springcloud.entities.CommonResult;
 import cn.guo.springcloud.entities.Payment;
 import cn.guo.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,15 +23,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "payment/create" )
     public CommonResult<Integer> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("***** 插入结果： "+result);
 
         if (result>0){
-            return new CommonResult<>(200,"插入数据库成功",result);
+            return new CommonResult<>(200,"插入数据库成功,serverPort:  "+serverPort,result);
         }else {
-            return new CommonResult<Integer>(444,"插入数据库失败",null);
+            return new CommonResult<Integer>(444,"插入数据库失败,serverPort:  "+serverPort,null);
         }
     }
 
@@ -42,9 +46,9 @@ public class PaymentController {
         log.info("***** 查询结果："+payment);
 
         if (payment!=null){
-            return new CommonResult<>(200,"查询成功",payment);
+            return new CommonResult<>(200,"查询成功,serverPort:  "+serverPort,payment);
         }else {
-            return new CommonResult<>(444,"无对应记录"+id,null);
+            return new CommonResult<>(444,"无对应记录"+id+",serverPort:  "+serverPort,null);
         }
     }
 
