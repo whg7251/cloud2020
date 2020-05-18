a6b8a9e3d306e862b14be7aff047bdf2af1da3ee
guo <whg7251@163.com>
2020-05-18 11:41:31 +0800

    Eureka  server 主启动类

---
+++ cloud-eureka-server7001/src/main/java/cn/guo/springcloud/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.guo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName EurekaServerApplication
+ * @Description
+ * @Author Guo
+ * @Date 2020/5/18 11:40
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
