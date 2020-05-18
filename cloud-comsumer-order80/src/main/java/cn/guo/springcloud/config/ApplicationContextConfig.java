ebadb31191a86d733b444690e6aaf2f98b5fa476
guo <whg7251@163.com>
2020-05-19 00:04:12 +0800

    RestTemplate 添加@LoadBalance负载均衡注解

--- cloud-comsumer-order80/src/main/java/cn/guo/springcloud/config/ApplicationContextConfig.java
+++ cloud-comsumer-order80/src/main/java/cn/guo/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package cn.guo.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,7 @@ public class ApplicationContextConfig {
      * @return RestTemplate 对象注册
      */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
